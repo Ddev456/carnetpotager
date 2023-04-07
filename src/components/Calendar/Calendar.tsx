b37c367020fc5d5daea15920c988b4d1aeca3a65
dylan delhalle <dev.delhalle@gmail.com>
2023-04-07 19:04:58 +0200

    fix slow calendar loading issues

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,24 +1,14 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import { useQuery } from '@tanstack/react-query';
-import { client } from '~/lib/client/client';
-import { toast } from 'react-hot-toast';
-import { EventsScheme } from '~/lib/scheme/events';
-import { Loader } from '../Loader';
-import { Error } from '../Error';
+import { type EventType } from '~/lib/scheme/events';
 import { EmptyCalendar } from './EmptyCalendar';
 import { WizardModal } from '../WizardModal';
 import frLocale from '@fullcalendar/core/locales/fr';
 import listPlugin from "@fullcalendar/list";
 import multiMonthPlugin from '@fullcalendar/multimonth';
 
-const notifyFailed = () => toast.error("Couldn't fetch events...");
-
-const getAllEvents = async (signal?: AbortSignal) =>
-  client(`/api/events`, { signal, zodSchema: EventsScheme });
-
-export const Calendar = () => {
+export const Calendar = ({events}: {events: EventType[]}) => {
   const buildToolbar = () => {
     return {
       left: 'prev,next today',
@@ -28,21 +18,7 @@ export const Calendar = () => {
   };
   const [showModal, setShowModal] = useState(false);
   const enabled = typeof window !== "undefined" ? localStorage.getItem("isCalendarEmpty") : null;
-  const { data, isLoading, isError, refetch } = useQuery({
-    queryKey: ['events'],
-    queryFn: ({ signal }) => getAllEvents(signal),
-    onError: () => {
-        notifyFailed();
-    },
-    enabled: enabled === "true" ? false : true
-  });
-  if (isLoading) {
-    return <Loader />;
-    }
-
-    if (isError) {
-    return <Error error="Couldn't fetch Events..." reset={() => refetch()} />;
-    }
+  
   return (
     <>
     <WizardModal showModal={showModal} setShowModal={setShowModal}/>
@@ -50,7 +26,7 @@ export const Calendar = () => {
     <FullCalendar
     locale={frLocale}
     headerToolbar={buildToolbar()}
-    events={data.events}
+    events={events}
     plugins={[ dayGridPlugin, listPlugin, multiMonthPlugin ]}
     height={'100%'}
     initialView="dayGridMonth"
--- src/pages/calendrier/index.tsx
+++ src/pages/calendrier/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Calendar } from '~/components/Calendar/Calendar';
+import { type EventType } from '~/lib/scheme/events';
+import { getAllEvents } from '~/server/event';
 
-const Calendrier = () => {
+const Calendrier = ({events}: {events: EventType[]}) => {
 
   return (
     <>
-        <Calendar /> 
+        <Calendar events={events} /> 
     </>
   );
 };
+
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+  return { 
+  props: { 
+      events: JSON.parse(JSON.stringify(events)) 
+      } 
+  };
+}
 
 export default Calendrier;
